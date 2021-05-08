e8d2206222d43db4e1865f9d8378fb7e38273fd3
Chris Wilding <hello@chriswilding.co.uk>
2021-05-08 16:33:50 +0100

    Add terraform-github

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "4.9.3"
+    }
+  }
+}
+
+provider "github" {
+  owner = "ChrisWilding"
+}
+
+resource "github_repository" "terraform-github" {
+  name = "terraform-github"
+
+  visibility = "private"
+
+  allow_merge_commit     = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Terraform"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
