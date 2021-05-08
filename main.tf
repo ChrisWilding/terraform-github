83abc7d1a47973f9659341363804b46603dd60a4
Chris Wilding <hello@chriswilding.co.uk>
2021-05-08 16:35:33 +0100

    Update aws-nuke

--- main.tf
+++ main.tf
@@ -19,7 +19,6 @@ resource "github_repository" "aws-nuke" {
   allow_merge_commit     = false
   auto_init              = true
   delete_branch_on_merge = true
-  gitignore_template     = "Terraform"
   has_issues             = false
   has_projects           = false
   has_wiki               = false
