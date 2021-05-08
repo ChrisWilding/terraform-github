607a4fe9d06107d62c13011825a8a1b7a98dc509
Chris Wilding <hello@chriswilding.co.uk>
2021-05-08 16:34:10 +0100

    Add aws-nuke

--- main.tf
+++ main.tf
@@ -11,6 +11,22 @@ provider "github" {
   owner = "ChrisWilding"
 }
 
+resource "github_repository" "aws-nuke" {
+  name = "aws-nuke"
+
+  visibility = "private"
+
+  allow_merge_commit     = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Terraform"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
+
 resource "github_repository" "terraform-github" {
   name = "terraform-github"
 
