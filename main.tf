ea77e4a72b541cbd5f404eec1c27700ed03c54de
Chris Wilding <hello@chriswilding.co.uk>
2021-07-06 19:46:51 +0100

    Move repositories to separate file

--- main.tf
+++ main.tf
@@ -10,42 +10,11 @@ terraform {
   required_providers {
     github = {
       source  = "integrations/github"
-      version = "4.9.3"
+      version = "> 4.9.0"
     }
   }
 }
 
 provider "github" {
   owner = "ChrisWilding"
-}
-
-resource "github_repository" "aws-nuke" {
-  name = "aws-nuke"
-
-  visibility = "private"
-
-  allow_merge_commit     = false
-  auto_init              = true
-  delete_branch_on_merge = true
-  has_issues             = false
-  has_projects           = false
-  has_wiki               = false
-  license_template       = "apache-2.0"
-  vulnerability_alerts   = true
-}
-
-resource "github_repository" "terraform-github" {
-  name = "terraform-github"
-
-  visibility = "private"
-
-  allow_merge_commit     = false
-  auto_init              = true
-  delete_branch_on_merge = true
-  gitignore_template     = "Terraform"
-  has_issues             = false
-  has_projects           = false
-  has_wiki               = false
-  license_template       = "apache-2.0"
-  vulnerability_alerts   = true
 }
---
+++ repositories.tf
@@ -0,0 +1,47 @@
+
+resource "github_repository" "aws_nuke" {
+  name = "aws-nuke"
+
+  visibility = "private"
+
+  allow_merge_commit     = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
+
+resource "github_repository" "terraform_aws" {
+  name = "terraform-aws"
+
+  visibility = "private"
+
+  allow_merge_commit     = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Terraform"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
+
+resource "github_repository" "terraform_github" {
+  name = "terraform-github"
+
+  visibility = "private"
+
+  allow_merge_commit     = false
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Terraform"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
