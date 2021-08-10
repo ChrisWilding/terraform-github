d1023c4fb97ba39837346f08289667f3d8472b61
Chris Wilding <hello@chriswilding.co.uk>
2021-08-10 19:56:53 +0100

    Add exercism-go

---
+++ repository_exercism_go.tf
@@ -0,0 +1,14 @@
+resource "github_repository" "exercism_go" {
+	name = "exercism-go"
+
+	allow_merge_commit     = false
+	archive_on_destroy     = true
+	auto_init              = true
+	delete_branch_on_merge = true
+	gitignore_template     = "Go"
+	has_issues             = false
+	has_projects           = false
+	has_wiki               = false
+	license_template       = "apache-2.0"
+	vulnerability_alerts   = true
+  }
