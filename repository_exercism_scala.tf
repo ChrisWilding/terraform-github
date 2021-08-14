b44e5d678403f842af628277215462c6eb522a93
Chris Wilding <hello@chriswilding.co.uk>
2021-08-14 22:12:35 +0100

    Update exercism-scala

---
+++ repository_exercism_scala.tf
@@ -0,0 +1,16 @@
+resource "github_repository" "exercism_scala" {
+  name = "exercism-scala"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "Solutions to Exercism exercises in Scala"
+  gitignore_template     = "Scala"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  homepage_url           = "https://exercism.io/tracks/scala"
+  license_template       = "MIT"
+  vulnerability_alerts   = true
+}
