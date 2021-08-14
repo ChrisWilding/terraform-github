2b19f0884f643d05aa31233d47a0b7df89aa48f7
Chris Wilding <hello@chriswilding.co.uk>
2021-08-14 19:38:28 +0100

    Add jinventory

---
+++ repository_jinventory.tf
@@ -0,0 +1,14 @@
+resource "github_repository" "jinventory" {
+  name = "jinventory"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Java"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
