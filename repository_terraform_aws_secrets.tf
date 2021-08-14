85fa4bba7db9ea090e80846b32c76a45cf762997
Chris Wilding <hello@chriswilding.co.uk>
2021-08-14 20:45:51 +0100

    Add terraform-aws-secrets

---
+++ repository_terraform_aws_secrets.tf
@@ -0,0 +1,16 @@
+resource "github_repository" "terraform_aws_secrets" {
+  name = "terraform-aws-secrets"
+
+  visibility = "private"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Terraform"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
