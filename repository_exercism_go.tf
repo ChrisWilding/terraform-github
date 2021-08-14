e4fa735b174159bbf3937d881a335b1072287115
Chris Wilding <hello@chriswilding.co.uk>
2021-08-14 22:12:30 +0100

    Update exercism-go

--- repository_exercism_go.tf
+++ repository_exercism_go.tf
@@ -1,14 +1,16 @@
 resource "github_repository" "exercism_go" {
-	name = "exercism-go"
+  name = "exercism-go"
 
-	allow_merge_commit     = false
-	archive_on_destroy     = true
-	auto_init              = true
-	delete_branch_on_merge = true
-	gitignore_template     = "Go"
-	has_issues             = false
-	has_projects           = false
-	has_wiki               = false
-	license_template       = "apache-2.0"
-	vulnerability_alerts   = true
-  }
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "Solutions to Exercism exercises in Go"
+  homepage_url           = "https://exercism.io/tracks/go"
+  gitignore_template     = "Go"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "MIT"
+  vulnerability_alerts   = true
+}
