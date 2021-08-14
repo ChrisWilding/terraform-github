a4846619b172faa8452bfecfc13de9d90a4dfd70
Chris Wilding <hello@chriswilding.co.uk>
2021-08-14 22:44:06 +0100

    Add advent-of-code-2018

---
+++ repository_advent_of_code_2018.tf
@@ -0,0 +1,17 @@
+resource "github_repository" "advent_of_code_2018" {
+  name = "advent-of-code-2018"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  archived               = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "Solutions to Advent of Code 2018"
+  gitignore_template     = "Elixir"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  homepage_url           = "https://adventofcode.com/2018"
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
