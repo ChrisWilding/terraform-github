c4e8c21640d83041a70a1af797f13100dbf01f4a
Chris Wilding <hello@chriswilding.co.uk>
2021-08-15 15:13:51 +0100

    Add merchants-guide-to-the-galaxy

---
+++ repository_merchants_guide_to_the_galaxy.tf
@@ -0,0 +1,16 @@
+resource "github_repository" "merchants_guide_to_the_galaxy" {
+  name = "merchants-guide-to-the-galaxy"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  archived               = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "Solution to the Merchant's Guide to the Galaxy exercise"
+  gitignore_template     = "Ruby"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
