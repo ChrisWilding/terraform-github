99cd81204215e55f6db21990a4ad511411278abc
Chris Wilding <hello@chriswilding.co.uk>
2021-08-15 12:49:09 +0100

    Add experiment-io-ts

---
+++ repository_experiment_io_ts.tf
@@ -0,0 +1,14 @@
+resource "github_repository" "experiment_io_ts" {
+  name = "experiment-io-ts"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Node"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "MIT"
+  vulnerability_alerts   = true
+}
