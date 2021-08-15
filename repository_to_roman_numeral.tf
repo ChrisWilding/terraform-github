b743b88c353be28b08cf42e18f8883dc2751d2dc
Chris Wilding <hello@chriswilding.co.uk>
2021-08-15 15:28:40 +0100

    Add to-roman-numeral

---
+++ repository_to_roman_numeral.tf
@@ -0,0 +1,15 @@
+resource "github_repository" "to_roman_numeral" {
+  name = "to-roman-numeral"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  archived               = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Node"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
