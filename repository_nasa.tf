a6608b018cd2df4b3769d99f67bf71db4da4d98a
Chris Wilding <hello@chriswilding.co.uk>
2021-08-15 15:17:36 +0100

    Add nasa

---
+++ repository_nasa.tf
@@ -0,0 +1,17 @@
+resource "github_repository" "nasa" {
+  name = "nasa"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  archived               = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "A demo React app using the NASA image and video library API"
+  gitignore_template     = "Ruby"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  homepage_url           = "https://cw-nasa.herokuapp.com/search"
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
