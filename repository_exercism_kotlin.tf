cecf3ad5b82787b51a715887014b04bfd68300d9
Chris Wilding <hello@chriswilding.co.uk>
2021-10-15 16:57:02 +0100

    Add exercism-kotlin

---
+++ repository_exercism_kotlin.tf
@@ -0,0 +1,16 @@
+resource "github_repository" "exercism_kotlin" {
+  name = "exercism-kotlin"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "Solutions to Exercism exercises in Kotlin"
+  gitignore_template     = "Kotlin"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  homepage_url           = "https://exercism.io/tracks/kotlin"
+  license_template       = "MIT"
+  vulnerability_alerts   = true
+}
