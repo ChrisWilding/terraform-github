1016e84f541d493009aeb3809729b404fb49cfc2
Chris Wilding <chris.wilding@cinch.co.uk>
2021-10-29 19:06:52 +0100

    Add exercism-ocaml

---
+++ repository_exercism_ocaml.tf
@@ -0,0 +1,16 @@
+resource "github_repository" "exercism_ocaml" {
+  name = "exercism-ocaml"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "Solutions to Exercism exercises in OCaml"
+  gitignore_template     = "OCaml"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  homepage_url           = "https://exercism.org/tracks/ocaml"
+  license_template       = "MIT"
+  vulnerability_alerts   = true
+}
