ea206c8d5ff2be518c1033acec1b2c8e439348b2
Chris Wilding <hello@chriswilding.co.uk>
2021-11-20 16:42:33 +0000

    Add advent-of-code-2021

---
+++ repository_advent_of_code_2021.tf
@@ -0,0 +1,17 @@
+resource "github_repository" "advent_of_code_2021" {
+  name = "advent-of-code-2021"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  archived               = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "Solutions to Advent of Code 2021"
+  gitignore_template     = "Go"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  homepage_url           = "https://adventofcode.com/2021"
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
