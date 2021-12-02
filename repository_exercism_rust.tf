5d4141c2d5ba96289ac5a54609f674df0fc4503d
Chris Wilding <hello@chriswilding.co.uk>
2021-12-02 21:31:44 +0000

    Add exercism-rust

---
+++ repository_exercism_rust.tf
@@ -0,0 +1,16 @@
+resource "github_repository" "exercism_rust" {
+  name = "exercism-rust"
+
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  description            = "Solutions to Exercism exercises in Rust"
+  gitignore_template     = "Rust"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  homepage_url           = "https://exercism.org/tracks/rust"
+  license_template       = "MIT"
+  vulnerability_alerts   = true
+}
