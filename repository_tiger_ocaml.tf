fa0d02cd2976d8d3ae8fb65abe6cbee91a4dd93d
Chris Wilding <hello@chriswilding.co.uk>
2022-04-09 13:30:09 +0100

    Add go and ocaml tiger repositories

---
+++ repository_tiger_ocaml.tf
@@ -0,0 +1,15 @@
+resource "github_repository" "tiger_ocaml" {
+  name = "tiger-ocaml"
+
+  allow_auto_merge       = true
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "OCaml"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
