89b9141501f26fb316f588a3f26f3e3ac41beeab
Chris Wilding <hello@chriswilding.co.uk>
2023-03-02 21:07:33 +0000

    Add Powerful Command-Line Applications in Go (#20)

---
+++ repository_powerful_command_line_applications_in_go.tf
@@ -0,0 +1,15 @@
+resource "github_repository" "powerful_command_line_applications_in_go" {
+  name = "powerful-command-line-applications-in-go"
+
+  allow_auto_merge       = true
+  allow_merge_commit     = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  gitignore_template     = "Go"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  license_template       = "apache-2.0"
+  vulnerability_alerts   = true
+}
